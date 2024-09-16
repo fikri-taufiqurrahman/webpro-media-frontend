283001627fb5b158fe434432727f59fc440be77d
gaduhhartawan <garzent12@gmail.com>
2024-09-16 19:12:11 +0700

    feat: add button component

--- src/pages/auth/login/index.jsx
+++ src/pages/auth/login/index.jsx
@@ -1,7 +1,13 @@
+import { Button } from "@/components/ui/button";
 import React from "react";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div>
+      <h1 className="text-3xl text-center">Login Page</h1>
+      <Button>Login</Button>
+    </div>
+  );
 };
 
 export default Login;
