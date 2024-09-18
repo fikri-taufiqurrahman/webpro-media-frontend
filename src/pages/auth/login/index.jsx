d13a7d5b832983ab4253c407f0821c045162aa83
gaduhhartawan <garzent12@gmail.com>
2024-09-19 02:26:00 +0700

    feat: update using helmet

--- src/pages/auth/login/index.jsx
+++ src/pages/auth/login/index.jsx
@@ -2,38 +2,46 @@ import { Button } from "@/components/ui/button";
 import { Card } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { IconBrandGoogleFilled } from '@tabler/icons-react';
+import { IconBrandGoogleFilled } from "@tabler/icons-react";
 import React from "react";
 import { Link } from "react-router-dom";
 import { MENUS } from "@/constants/menus";
+import { Helmet } from "react-helmet-async";
+import HelmetProvider from "@/components/layouts/helmet-provider";
 
 const Login = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100 gap-6">
+      {/* Helmet */}
+      <HelmetProvider title="WebPro Media | Login" />
+
       <Card className="w-96 max-w-md p-6 bg-white rounded-lg shadow-md">
         <h1 className="text-3xl font-bold text-center mb-10">Web ProMedia</h1>
         <form className="space-y-4">
-          
-          <Label htmlFor="email" className="sr-only"/>
-          <Input 
-            type="email" 
-            id="email" 
+          <Label htmlFor="email" className="sr-only" />
+          <Input
+            type="email"
+            id="email"
             className="w-full p-2 border rounded-md "
             placeholder="Mobile Number, username, or email"
             required
           />
-          
-          <Label htmlFor="password" className="sr-only"/>
-          <Input 
-            type="password" 
-            id="password" 
+
+          <Label htmlFor="password" className="sr-only" />
+          <Input
+            type="password"
+            id="password"
             className="w-full p-2 border rounded-md "
             placeholder="Enter your password"
             required
           />
-          
-          
-          <Button type="submit" className="w-full bg-[#EF2B7CCC] text-white p-2 rounded-md">Log in</Button>
+
+          <Button
+            type="submit"
+            className="w-full bg-[#EF2B7CCC] text-white p-2 rounded-md"
+          >
+            Log in
+          </Button>
 
           <div className="relative flex items-center ">
             <hr className="w-full border-gray-300 my-2" />
@@ -41,20 +49,23 @@ const Login = () => {
               OR
             </span>
           </div>
-          
+
           <div className="flex items-center justify-center text-xl text-[#E53935] font-bold gap-3 pt-6">
-            <IconBrandGoogleFilled color="#E53935"/>
+            <IconBrandGoogleFilled color="#E53935" />
             <Link>Masuk dengan Google</Link>
           </div>
           <div className="flex items-center justify-center text-sm text-[#8B8181]">
             <Link>Forgot password?</Link>
           </div>
-          
         </form>
       </Card>
       <Card className="w-96 max-w-md p-6 bg-white rounded-lg shadow-md">
-        <div className="text-center  font-semibold">Don't have an account? <Link to={MENUS.REGISTER}><span className="text-[#EF2B7C]">Sign Up</span> </Link></div>
-        
+        <div className="text-center  font-semibold">
+          Don't have an account?{" "}
+          <Link to={MENUS.REGISTER}>
+            <span className="text-[#EF2B7C]">Sign Up</span>{" "}
+          </Link>
+        </div>
       </Card>
     </div>
   );
--- src/pages/auth/register/index.jsx
+++ src/pages/auth/register/index.jsx
@@ -5,76 +5,99 @@ import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Link } from "react-router-dom";
 import { MENUS } from "@/constants/menus";
+import HelmetProvider from "@/components/layouts/helmet-provider";
 
 const Register = () => {
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-gray-100 gap-6">
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 gap-6 py-10">
+      {/* Helmet */}
+      <HelmetProvider title="WebPro Media | Register" />
+
       <Card className="w-96 max-w-md p-6 bg-white rounded-lg shadow-md">
         <h1 className="text-3xl font-bold text-center mb-8">Web ProMedia</h1>
-        <h2 className="text-lg text-[#BEBDBD] font-semibold text-center mb-8">Sign up to see photos and videos <br /> from your friends.</h2>
+        <h2 className="text-lg text-[#BEBDBD] font-semibold text-center mb-8">
+          Sign up to see photos and videos <br /> from your friends.
+        </h2>
         <form className="space-y-4">
-          
-          <Label htmlFor="mobile_number" className="sr-only"/>
-          <Input 
-            type="number" 
-            id="mobile_number" 
+          <Label htmlFor="mobile_number" className="sr-only" />
+          <Input
+            type="number"
+            id="mobile_number"
             className="w-full p-2 border rounded-md "
             placeholder="Mobile Number"
             required
           />
-          
-          <Label htmlFor="email" className="sr-only"/>
-          <Input 
-            type="email" 
-            id="email" 
+
+          <Label htmlFor="email" className="sr-only" />
+          <Input
+            type="email"
+            id="email"
             className="w-full p-2 border rounded-md "
             placeholder="Email"
             required
           />
 
-          <Label htmlFor="name" className="sr-only"/>
-          <Input 
-            type="text" 
-            id="name" 
+          <Label htmlFor="name" className="sr-only" />
+          <Input
+            type="text"
+            id="name"
             className="w-full p-2 border rounded-md "
             placeholder="Full Name"
             required
           />
 
-          <Label htmlFor="username" className="sr-only"/>
-          <Input 
-            type="text" 
-            id="username" 
+          <Label htmlFor="username" className="sr-only" />
+          <Input
+            type="text"
+            id="username"
             className="w-full p-2 border rounded-md "
             placeholder="Username"
             required
           />
 
-          <Label htmlFor="password" className="sr-only"/>
-          <Input 
-            type="text" 
-            id="password" 
+          <Label htmlFor="password" className="sr-only" />
+          <Input
+            type="text"
+            id="password"
             className="w-full p-2 border rounded-md "
             placeholder="Password"
             required
           />
-          
+
           <div className="text-center text-xs font-medium">
-            <p>People who use our service may have uploaded <br /> your contact information to Instagram. <span className="underline underline-offset-1"><Link>Learn More</Link></span></p>
+            <p>
+              People who use our service may have uploaded <br /> your contact
+              information to Instagram.{" "}
+              <span className="underline underline-offset-1">
+                <Link>Learn More</Link>
+              </span>
+            </p>
           </div>
 
           <div className="text-center text-xs font-medium">
-            <p>By signing up, you agree to our <span className="underline underline-offset-1"><Link>Term, Privacy Policy and Cookies Policy</Link></span></p>
+            <p>
+              By signing up, you agree to our{" "}
+              <span className="underline underline-offset-1">
+                <Link>Term, Privacy Policy and Cookies Policy</Link>
+              </span>
+            </p>
           </div>
-          
-          <Button type="submit" className="w-full bg-[#EF2B7CCC] text-white p-2 rounded-md">Sign Up</Button>
-          
-          
+
+          <Button
+            type="submit"
+            className="w-full bg-[#EF2B7CCC] text-white p-2 rounded-md"
+          >
+            Sign Up
+          </Button>
         </form>
       </Card>
       <Card className="w-96 max-w-md p-6 bg-white rounded-lg shadow-md">
-        <div className="text-center  font-semibold">Have an account? <Link to={MENUS.LOGIN}><span className="text-[#EF2B7C]">Log In</span> </Link></div>
-        
+        <div className="text-center  font-semibold">
+          Have an account?{" "}
+          <Link to={MENUS.LOGIN}>
+            <span className="text-[#EF2B7C]">Log In</span>{" "}
+          </Link>
+        </div>
       </Card>
     </div>
   );
